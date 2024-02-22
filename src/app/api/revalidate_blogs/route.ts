4b1cb4b7d35f1516c178e83c628c27b60f716cb9
Joon Seo An <joonseo.an@outlook.com>
2024-02-22 09:13:04 -0500

    adding tag2

--- src/app/api/revalidate_blogs/route.ts
+++ src/app/api/revalidate_blogs/route.ts
@@ -2,10 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 // import blogs from '@/contents/blogs.json';
 // import { delay } from "@/utils";
 
+import { revalidateTag } from "next/cache";
+
 
 // For `revalidate` we will use POST.
 // `secret` is for authorization. For instance, only admin can revalidate the page.
 // http://localhost:3000/api/revalidate_blogs?tag=blogs&secret=afafdafdad
 export async function POST(request: NextRequest) {
-  return NextResponse.json({ data: 'should work' });
+  // [IMPORTANT]
+  // The way to get params in nextjs
+  // http://localhost:3000/api/revalidate_blogs?tag=blogs&secret=afafafa
+  const tag = request.nextUrl.searchParams.get('tag');
+  console.log('tag: ', tag)
+  const secret = request.nextUrl.searchParams.get('secret');
+  console.log('secret: ', secret)
+
+  if (tag) {
+    revalidateTag(tag);
+  } else if (!(tag && secret)) {
+    return NextResponse.json({ message: 'Invalid Request' }, { status: 400 });
+  }
+
+  if (secret !== process.env.REVALIDATE_TOKEN) {
+    return NextResponse.json({ message: 'Invalid Request' }, { status: 401 });
+  }
+
+  return NextResponse.json({ revalidated: true, now: Date.now() });
 }
