e2eea64f1e33c593dedadfe8609aa2d51aae451e
joon <joonseo.an@outlook.com>
2024-03-21 01:30:20 -0400

    done

--- src/utils/fetching.json.content.ts
+++ src/utils/fetching.json.content.ts
@@ -2,23 +2,10 @@ import { TestData } from "@/app/page";
 import blogs from '@/contents/blogs.json';
 import portfolios from '@/contents/portfolio.json';
 
-export function getBlogs<T>(): T {
-  // const res = await fetch('http://localhost:4000/api/blogs');
-
-  // if (!res.ok) {
-  //   throw new Error('Unable to get blogs');
-  // }
-
+export function getBlogs<T extends TestData[]>(): T {
   return blogs as T;
 }
 
-export function getPortfolios<T>(): T {
-  
-  // const res = await fetch('http://localhost:4000/api/portfolios');
-
-  // if (!res.ok) {
-  //   throw new Error('Unable to get Portfolios');
-  // }
-
+export function getPortfolios<T extends TestData[]>(): T {
   return portfolios as T;
 }
